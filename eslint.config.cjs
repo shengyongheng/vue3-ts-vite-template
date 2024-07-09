24bf1264bb80582951485eee798101259c8321c7
hsy <3271660881@qq.com>
2024-07-09 23:27:42 +0800

    feat(eslint-config): eslint 自动修复

---
+++ eslint.config.cjs
@@ -0,0 +1,100 @@
+// 默认生成配置
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+// import pluginVue from "eslint-plugin-vue";
+
+// export default [
+//   { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+//   { languageOptions: { globals: globals.browser } },
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   ...pluginVue.configs["flat/essential"],
+// ];
+
+// 自定义配置
+// eslint.config.js
+const pluginVue = require("eslint-plugin-vue");
+const vueParser = require("vue-eslint-parser");
+const typescriptParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    ignores: [
+      "**/node_modules/**/*",
+      "**/dist/**/*",
+      "**/lib/**/*",
+      "**/types/**/*.d.ts",
+    ],
+    files: [
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.vue",
+      "**/*.js",
+      "**/*.jsx",
+      "**/*.cjs",
+    ],
+  },
+  {
+    plugins: { typescriptParser },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue", ".ts", ".tsx", "css"],
+      },
+    },
+  },
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        sourceType: "module",
+        jsx: true,
+      },
+    },
+  },
+  {
+    rules: {
+      "linebreak-style": ["off", "lf"], // 换行符
+      quotes: ["error", "double"], // 使用双引号
+      eqeqeq: ["error", "smart"], // 比较的时候使用严格等于
+      "no-unused-vars": "off", // 禁止出现未使用过的变量
+      "default-case": "error", // 要求 switch 语句中有 default 分支
+      // "brace-style": ["error", "stroustrup", { allowSingleLine: true }], // 大括号风格 ["error", "stroustrup"]
+      // "no-dupe-keys": "error", // 对象中不允许出现重复的键
+      // "no-sparse-arrays": "error", // 禁止稀疏数组， [1,,2]
+      // "no-empty": "error", // 不允许出现空的代码块
+      // "@typescript-eslint/no-explicit-any": "off", // 允许any类型
+      // "block-scoped-var": "error", // 将变量声明放在合适的代码块里
+      // curly: ["error", "all"], // 强制使用花括号的风格
+      // "no-self-compare": "error", // 不允许自身比较
+      // "no-multiple-empty-lines": ["error", { max: 2 }], // 空行最多不能超过两行
+      // "no-const-assign": "error", // 禁止修改const声明的变量
+      // "no-redeclare": "error", // 禁止重复声明变量
+      // "no-func-assign": "error", // 禁止重复的函数声明
+      // "no-shadow": "error", // 外部作用域中的变量不能与它所包含的作用域中的变量或参数同名
+      // // 空格
+      // "space-infix-ops": ["error", { int32Hint: true }], // 操作符周围的空格
+      // "space-before-function-paren": [
+      //   "error",
+      //   { anonymous: "never", named: "never", asyncArrow: "always" },
+      // ], // 函数定义时括号前的空格
+      // "space-before-blocks": ["error", "always"], // 在块语句之前始终有一个空格
+      // "template-curly-spacing": ["off", "never"], // 要求模板字符串中的嵌入表达式周围空格的使用
+      // "key-spacing": ["error", { beforeColon: false, afterColon: true }], // 对象字面量中冒号的前后空格
+      // "array-bracket-spacing": ["off", "always"], // 数组内前后要求有空格
+      // "object-curly-spacing": ["error", "always"], // 对象内前后要求有空格
+      // "arrow-spacing": ["error", { before: true, after: true }], // 前头=> 前后都有空格
+      // "comma-spacing": ["error", { before: false, after: true }], // 要求同一行内逗号后面有空格
+      // "keyword-spacing": "error", // 关键字前后的空格
+      // "no-trailing-spaces": "error", // 一行最后不允许有空格
+      // "switch-colon-spacing": ["error", { before: false, after: true }], // switch 冒号后要有空格
+      // "no-multi-spaces": "error", // 不允许出现多余的空格
+    },
+  },
+];
--- src/views/antd/pages/table/index.vue
+++ src/views/antd/pages/table/index.vue
@@ -1,74 +1,104 @@
 <template>
-  <el-table ref="multipleTableRef" :data="dataSource.data" style="width: 100%" @sort-change="handleSortChange"
-    @filter-change="handleFilterChange">
-    <el-table-column v-for="column in computedColumns" :key="column.prop" :prop="column.prop" :label="column.label"
-      :width="column.width" :type="column.type" :filters="column.filters" :sortable="column.sortable"
-      :sort-orders="column.sortOrders" :column-key="column.columnKey" :filter-multiple="column.filterMultiple">
-      <template #default="scope" v-if="column.slot">
-        <slot :name="column.slot" :row="scope.row"></slot>
+  <el-table
+    ref="multipleTableRef"
+    :data="dataSource.data"
+    style="width: 100%"
+    @sort-change="handleSortChange"
+    @filter-change="handleFilterChange"
+  >
+    <el-table-column
+      v-for="column in computedColumns"
+      :key="column.prop"
+      :prop="column.prop"
+      :label="column.label"
+      :width="column.width"
+      :type="column.type"
+      :filters="column.filters"
+      :sortable="column.sortable"
+      :sort-orders="column.sortOrders"
+      :column-key="column.columnKey"
+      :filter-multiple="column.filterMultiple"
+    >
+      <template
+        v-if="column.slot"
+        #default="scope"
+      >
+        <slot
+          :name="column.slot"
+          :row="scope.row"
+        />
       </template>
     </el-table-column>
   </el-table>
-  <el-pagination v-if="dataSource?.pigination" :currentPage="dataSource?.pigination?.currentPage"
-    :page-size="dataSource?.pigination?.pageSize" :page-sizes="[1, 2, 3, 4, 5]"
-    layout="total, sizes, prev, pager, next, jumper" :total="dataSource?.pigination?.total"
-    @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+  <el-pagination
+    v-if="dataSource?.pigination"
+    :current-page="dataSource?.pigination?.currentPage"
+    :page-size="dataSource?.pigination?.pageSize"
+    :page-sizes="[1, 2, 3, 4, 5]"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="dataSource?.pigination?.total"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+  />
 </template>
 
 <script lang="ts" setup>
-import { ElTable } from 'element-plus'
-import { computed, onMounted, reactive, ref } from 'vue'
-import { ITableProps, ISortChangeParams, IFilterChangeParams } from './type'
+import { ElTable } from "element-plus";
+import { computed, onMounted, reactive, ref } from "vue";
+import { ITableProps, ISortChangeParams, IFilterChangeParams } from "./type";
 interface IProps {
-  tableRef?: InstanceType<typeof ElTable>
-  dataSource: ITableProps<any>
+  tableRef?: InstanceType<typeof ElTable>;
+  dataSource: ITableProps<any>;
 }
-const { dataSource } = defineProps<IProps>()
+const { dataSource } = defineProps<IProps>();
+defineOptions({
+  name: "MyTable",
+});
 const emits = defineEmits<{
-  (event: 'sizeChange', pageSize: number): void
-  (event: 'currentChange', currentPage: number): void
-  (event: 'sortChange', data: ISortChangeParams): void
-  (event: 'filterChange', data: IFilterChangeParams): void
-}>()
+  (event: "sizeChange", pageSize: number): void;
+  (event: "currentChange", currentPage: number): void;
+  (event: "sortChange", data: ISortChangeParams): void;
+  (event: "filterChange", data: IFilterChangeParams): void;
+}>();
 
-const multipleTableRef = ref<InstanceType<typeof ElTable>>()
+const multipleTableRef = ref<InstanceType<typeof ElTable>>();
 
 const filters = reactive<IFilterChangeParams>({});
-
+var a = 1;
 onMounted(() => {
   if (dataSource.columns) {
-    dataSource.columns.forEach(item => {
+    dataSource.columns.forEach((item) => {
       if (item.columnKey) {
-        filters[item.columnKey] = []
+        filters[item.columnKey] = [];
       }
-    })
+    });
   }
-})
+});
 
 const computedColumns = computed(() =>
-  dataSource.columns.filter(item => !item.hidden)
-)
+  dataSource.columns.filter((item) => !item.hidden)
+);
 const handleSizeChange = (pageSize: number) => {
-  console.log(pageSize, 'pageSize-Table')
-  emits('sizeChange', pageSize)
-}
+  console.log(pageSize, "pageSize-Table");
+  emits("sizeChange", pageSize);
+};
 const handleCurrentChange = (currentPage: number) => {
-  console.log(currentPage, 'currentPage-Table')
-  emits('currentChange', currentPage)
-}
+  console.log(currentPage, "currentPage-Table");
+  emits("currentChange", currentPage);
+};
 
 const handleSortChange = (data: ISortChangeParams) => {
-  console.log(data, 'sort-Table')
-  emits('sortChange', data)
-}
+  console.log(data, "sort-Table");
+  emits("sortChange", data);
+};
 
 const handleFilterChange = (filter: IFilterChangeParams) => {
-  const newfilters = { ...filters, ...filter }
-  filters[Object.keys(filter)[0]] = filter[Object.keys(filter)[0]]
-  emits('filterChange', newfilters)
-}
+  const newfilters = { ...filters, ...filter };
+  filters[Object.keys(filter)[0]] = filter[Object.keys(filter)[0]];
+  emits("filterChange", newfilters);
+};
 
 defineExpose({
   multipleTableRef,
-})
+});
 </script>
